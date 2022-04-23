8d092b23a4eae188ea98cce99de525374ac2f753
YI JIN <kayjinyi@gmail.com>
2022-04-23 14:00:18 -0700

    update

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # teamProfile
 
 AS A manager
+
 I WANT to generate a webpage that displays my team's basic info. SO THAT I have quick access to their emails and GitHub profiles
 
 GIVEN a command-line application that accepts user input
--- index.js
+++ index.js
@@ -1,74 +1,61 @@
 console.log("linked!");
 const inquirer = require("inquirer");
 const fs = require("fs");
-//README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-
+const { exit } = require("process");
+//prompted to enter the team manager’s name, employee ID, email address, and office number
 inquirer
   .prompt([
-    { type: "input", name: "title", message: "what is your Project title?" },
-
-    { type: "input", name: "description", message: "what is description" },
     {
       type: "input",
-      name: "installation",
-      message: "what is installation instructions",
+      name: "managerName",
+      message: "what is your Manager's name?",
     },
-    { type: "input", name: "usage", message: "what is usage information" },
+
+    { type: "input", name: "managerID", message: "what is your Manager's ID" },
+
     {
       type: "input",
+      name: "managerEmail",
+      message: "what is your Manager's email",
+    },
+    {
+      type: "input",
+      name: "managerOffice",
+      message: "what is your Manager's office number",
+    },
+    {
+      type: "choices",
       name: "contribution",
       message: "what is your contribution guidelines?",
     },
-    { type: "input", name: "test", message: "what is your test instruction?" },
     {
       type: "list",
-      message: "What is your license?",
-      name: "license",
-      choices: ["MIT", "ISC", "Unlicense"],
+      message: "Which type of team member would you like to add",
+      name: "typeOfTeam",
+      choices: ["engineer", "intern", "finish building"],
     },
-    {
-      type: "input",
-      name: "username",
-      message: "what is your Github username?",
-    },
-
-    { type: "input", name: "email", message: "what is your email?" },
   ])
   .then((data) => {
     console.log(data);
-    const {
-      title,
-      description,
-      installation,
-      usage,
-      contribution,
-      test,
-      license,
-      username,
-      email,
-    } = data;
-    let licenseC;
-    switch (license) {
-      case "MIT":
-        licenseC =
-          "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-      case "ISC":
-        licenseC =
-          "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
-        break;
-      case "Boost Software License 1.0":
-        licenseC =
-          "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
-        break;
-      default:
-        licenseC = "";
-    }
+    // switch (data.typeOfTeam) {
+    //   case "engineer":
+    //     addEngineer();
+    //     break;
+    //   case "intern":
+    //     addIntern();
+    //     break;
+    //   case "finish building":
+    //    exit();
+    //     break;
+    //   default:
+    //     break;
+    // }
 
     const content = `
-
+${data.managerEmail}
 `;
 
-    fs.writeFile("index.html", content, (err) =>
+    fs.writeFile("./dist/index.html", content, (err) =>
       err ? console.log(err) : console.log("Success")
     );
   });
