542adc9aa6813f2389157cd0fdc1bcf9609aafd1
YI JIN <kayjinyi@gmail.com>
2022-04-24 03:26:19 -0700

    update

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -2,7 +2,7 @@ const Employee = require("../lib/Employee");
 
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
-  expect(typeof(e)).toBe("object");
+  expect(typeof e).toBe("object");
 });
 
 test("Can set name via constructor arguments", () => {
@@ -41,7 +41,7 @@ test("Can get email via getEmail()", () => {
   expect(e.getEmail()).toBe(testValue);
 });
 
-test("getRole() should return \"Employee\"", () => {
+test('getRole() should return "Employee"', () => {
   const testValue = "Employee";
   const e = new Employee("Alice", 1, "test@test.com");
   expect(e.getRole()).toBe(testValue);
